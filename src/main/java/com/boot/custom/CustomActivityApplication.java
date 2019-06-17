ce14e893cf10f7bc4701c0899d73840a5a4de633
Maicon Gil <gugamaicon@gmail.com>
2019-06-17 16:08:16 -0300

    created spring boot project

---
+++ src/main/java/com/boot/custom/CustomActivityApplication.java
@@ -0,0 +1,13 @@
+package com.boot.custom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomActivityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomActivityApplication.class, args);
+	}
+
+}
