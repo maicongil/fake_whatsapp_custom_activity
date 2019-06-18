2049c03b91541643d0bbbf46e362e05b799f7ef9
Maicon Gil <gugamaicon@gmail.com>
2019-06-17 22:06:26 -0300

    updated backend code

---
+++ src/main/java/com/boot/custom/ActivityResult.java
@@ -0,0 +1,14 @@
+package com.boot.custom;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ActivityResult {
+
+    private String branchResult;
+
+}
